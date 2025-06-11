6a4df14a01d09bd5359eead3a1bfc8369e834593
Jean Teissier <jeanteissier78@gmail.com>
2025-06-11 14:43:18 +0000

    Cleaned up k3s_install script

--- p1/install_k3s.sh
+++ p1/install_k3s.sh
@@ -11,7 +11,6 @@ if [[ -z "$1" ]]; then
 	exit 1;
 fi
 
-sudo rm -rf /vagrant_shared/*
 UFW_STATUS="$(sudo ufw status)"
 if echo "$UFW_STATUS" | grep -q "Status: active"; then
 	echo "INSTALL_K3S: Disabling UFW"
@@ -20,45 +19,42 @@ fi
 
 # K3s
 MODE="$1"
-AGENT_SLEEP=0
 SERVER_IP="192.168.56.110"
 if  [ "$MODE" = "server" ]; then
 	echo "INSTALL_K3S: Installing k3s server"
 	sudo curl -sfL https://get.k3s.io |INSTALL_K3S_SKIP_START=true sh -s - --node-ip=$SERVER_IP --advertise-address=$SERVER_IP
 	sudo systemctl enable k3s
 	sudo systemctl start k3s
-	mkdir -p /vagrant_shared
-	echo "INSTALL_K3S: Copying token"
+	echo "SERVER:INSTALL_K3S: Copying token"
 	sudo cp /var/lib/rancher/k3s/server/node-token /vagrant_shared/node-token
-	echo "INSTALL_K3S: Printing original token"
-	sudo cat /var/lib/rancher/k3s/server/node-token
-	echo "INSTALL_K3S: Printing copy token"
-	sudo cat /vagrant_shared/node-token
+	echo "SERVER:INSTALL_K3S: Done!"
 elif [ "$MODE" = "agent" ]; then
 	for i in {1..30}; do
 		if mount | grep -q "/vagrant_shared"; then
 			break
 		fi
-		echo "INSTALL_K3S: Waiting for /vagrant_shared to be mounted..."
+		echo "SERVER:INSTALL_K3S: Waiting for /vagrant_shared to be mounted..."
 		sleep 1
 	done
-	echo "INSTALL_K3S: Installing k3s agent"
-	for i in {1..1000}; do
+	echo "SERVER:INSTALL_K3S: Installing k3s agent"
+	for i in {1..100}; do
 		if [ -f /vagrant_shared/node-token ]; then
 			break
 		fi
-		echo "INSTALL_K3S: Waiting for node-token..."
+		echo "SERVER:INSTALL_K3S: Waiting for node-token..."
 		sleep 2
 	done
 	if [ ! -f /vagrant_shared/node-token ]; then
-		echo "INSTALL_K3S: Timeout waiting for server token"
+		echo "SERVER:INSTALL_K3S: Timeout waiting for server token"
 		exit 1
 	fi
-	echo "INSTALL_K3S: Found token, installing"
+	echo "SERVER:INSTALL_K3S: Found token, installing"
 	export K3S_URL=https://$SERVER_IP:6443
 	export K3S_TOKEN=$(cat /vagrant_shared/node-token)
+	sudo rm -rf /vagrant_shared/*
 	sudo curl -sfL https://get.k3s.io | sh -s - --node-ip=192.168.56.111 
+	echo "SERVER:INSTALL_K3S: Done!"
 else
-	echo "INSTALL_K3S: Unrecognized installation mode: $MODE"
+	echo "SERVER:INSTALL_K3S: Unrecognized installation mode: $MODE"
 	exit 1;
 fi
