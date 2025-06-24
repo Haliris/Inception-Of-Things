941cb3bd31d523fbd8ecd779d72bc3611a1fcc0b
Jean Teissier <jeanteissier78@gmail.com>
2025-06-24 20:14:00 +0000

    Base done for argocd install script

--- p3/configure_cluster.sh
+++ p3/configure_cluster.sh
@@ -2,9 +2,15 @@
 
 set -e
 
-echo "Creating cluster..."
+echo "CONFIGURE_CLUSTER: Checking dependencies..."
+bash install_k3d.sh
+echo "CONFIGURE_CLUSTER: Creating cluster"
 sudo k3d cluster create mycluster
-echo "Installing argocd..."
+echo "CONFIGURE_CLUSTER: Installing argocd..."
 sudo kubectl create namespace argocd
 sudo kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-echo "Done!"
+echo "CONFIGURE_CLUSTER: Waiting for argocd pods..."
+sudo kubectl wait --for=condition=Ready pods --all -n argocd --timeout=380s
+echo "CONFIGURE_CLUSTER: Forwarding port"
+sudo kubectl port-forward svc/argocd-server -n argocd 8080:443 &
+echo "CONFIGURE_CLUSTER: Done!"
