4aeacbb54ab06653581f4dfd549836dbc68f86d5
Jean Teissier <jeanteissier78@gmail.com>
2025-06-24 20:46:05 +0000

    Wil app basic yaml files work, need to configure argo correctly

--- p3/configure_cluster.sh
+++ p3/configure_cluster.sh
@@ -13,4 +13,9 @@ echo "CONFIGURE_CLUSTER: Waiting for argocd pods..."
 sudo kubectl wait --for=condition=Ready pods --all -n argocd --timeout=380s
 echo "CONFIGURE_CLUSTER: Forwarding port"
 sudo kubectl port-forward svc/argocd-server -n argocd 8080:443 &
+echo "CONFIGURE_CLUSTER: Installing Wil app..."
+sudo kubectl create namespace dev
+sudo kubectl apply -n dev -f k8s/
+echo "CONFIGURE_CLUSTER: Waiting for dev pods..."
+sudo kubectl wait --for=condition=Ready pods --all -n dev --timeout=100s
 echo "CONFIGURE_CLUSTER: Done!"
